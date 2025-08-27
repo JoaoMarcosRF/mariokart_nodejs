df05d93fda56a2b69c51afe608f9edf5e22714e2
JoaoMarcosRF <jmarcs.rf@gmail.com>
2025-08-27 10:55:48 -0300

    add readme and docs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("oiodkids")
+const player00 = {
+    nome : "MARIO" , 
+    velocidade : 4, 
+    manobrabilidade : 3,
+    poder :  3,
+    pontos :  0,
+};
+
+const player01 = {
+    nome : "LUIGI" , 
+    velocidade : 4, 
+    manobrabilidade : 3,
+    poder :  3,
+    pontos :  0,
+};
+
+async function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+}
